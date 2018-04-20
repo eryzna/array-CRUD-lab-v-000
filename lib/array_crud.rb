635466b6447c0716791c7379122cf500785131c1
Elana Ryznar <eryzna@gmail.com>
2018-04-20 23:11:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array=pets_array
+  array=pets_array= ["cats", "dogs", "birds", "ferrets"]
   element="reptiles"
   pets_array << "reptiles"
 end
